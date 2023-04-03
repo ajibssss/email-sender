157c491ccf5df37358ce423018d3b32221a5b058
ajibssss <ajibadetolu23@gmail.com>
2023-04-03 16:00:28 +0100

    m

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
